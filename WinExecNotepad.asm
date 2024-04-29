de1521c8217538b29266657d2c5569e7c05aaa31
max <40187297+maxbarkouras@users.noreply.github.com>
2024-04-29 13:34:15 -0700

    Update WinExecNotepad.asm

--- WinExecNotepad.asm
+++ WinExecNotepad.asm
@@ -5,51 +5,46 @@ global _start
 
 _start:
 
-    ;kernel32 base poi(poi(poi(poi($peb+0x18)+0x20)-10))
-    ;sub rsp, 0xfffffffffffffdf8
+    ; FIND AND CALL WINEXEC
 
-    sub rsp, 0x28                 ; 40 bytes of shadow space
-    and rsp, 0FFFFFFFFFFFFFFF0h
+    sub rsp, 0x28                    ; create shadow space on stack
+    and rsp, 0FFFFFFFFFFFFFFF0h   
 
-    xor rax, rax
+    xor rax, rax                     ; clear registers
     xor rbx, rbx
     xor rcx, rcx
-    mov rax, [gs:60h + rbx] ;set rax to PEB
-    mov rax, [rax+18h] ;set rbx to LDR
-    mov rax, [rax+20h] ;set rcx to module list
+    mov rax, [gs:60h + rbx]          ; get kernel32.dll address
+    mov rax, [rax+18h]            
+    mov rax, [rax+20h]            
     mov rax, [rax+rcx]
-    mov rax, [rax+rcx] ;kernel32.dll
-    mov rbx, [rax+20h] ;kernel32 base addr to r9
-    mov r8, rbx
+    mov rax, [rax+rcx]
+    mov rbx, [rax+20h]               ; move kernel32.dll address to rbx
+    mov r8, rbx                      ; move kernel32.dll to r8
     mov ebx, [rbx+3ch]
-    add rbx, r8 ;PE Addres
+    add rbx, r8                      ; move PE address into rbx
     mov rcx, 0x88FFFFFFFFFFFFFF
     shr rcx, 78h
     mov edx, [rbx + rcx]	
-    add rdx, r8 ;export table addr
-    mov r10d, [rdx + 14h] ;num of functions to r10
-    xor r11, r11
-    mov r11d, [rdx + 20h] ;function addr to r11
+    add rdx, r8                      ; move export table addr into rdx
+    mov r10d, [rdx + 14h]            ; move number of functions to r10
+    xor r11, r11   
+    mov r11d, [rdx + 20h]            ; move address of function names to r11
     add r11, r8
     xor rcx, rcx
 
-    ;r8 ntdll base addr
-    ;r10 number of functions
-    ;r11 addr of function names
-
-    mov rcx, r10
-    functionfinder:
+    mov rcx, r10                     ; move number of functions to rcx for counter
+    functionfinder:                  ; sort through address of function names until we find WinExec
         jecxz functionfound
         xor ebx, ebx
         mov ebx, [r11+4+rcx*4]
         add rbx, r8
         dec rcx
-        mov rax, 636578456E6957FFh
-        shr rax, 8h
-        cmp [rbx], rax
-        jnz functionfinder
+        mov rax, 636578456E6957FFh   ; move "WinExec"FF into rax
+        shr rax, 8h                  ; shorten string to remove FF, avoiding null bytes
+        cmp [rbx], rax               ; compare current function in loop with rax
+        jnz functionfinder           ; loop if current function is not "WinExec"
     
-    functionfound:
+    functionfound:                   ; when current function address is equal to "WinExec" get function from address
         xor r11, r11
         mov r11d, [rdx+0x24]
         add r11, r8
@@ -60,101 +55,91 @@ _start:
         add r11, r8
         mov eax, [r11+4+r13*4]
         add rax, r8
-        mov r14, rax
+        mov r14, rax                 ; move WinExec function into r14 
         
-        xor rax, rax      
-        mov rax, 657865FFFFFFFFFFh
+    xor rax, rax      
+    mov rax, 657865FFFFFFFFFFh       ; move "exe"FF into rax
+    shr rax, 68h                     ; shorten string to remove FF, avoiding null bytes
+    push rax                         ; push onto stack
+    mov rax, 'notepad.'              ; move "notepad." into rax, nasm can handle conversion to hex lol
+    push rax                         ; push onto stack
+    mov rcx, rsp                     ; move current stack, "notepad.exe", to rcx for parameter use
 
-        shr rax, 68h
-        push rax
-        mov rax, 'notepad.'
-        push rax
-        mov rcx, rsp
-        ;add rsp, 8h
-        ;add rsp, 8h
+    xor rdx,rdx
+    inc rdx                          ; set rdx to 1 for "SW_SHOWNORMAL" parameter
+    sub rsp, 0x30                    ; make stack space for call
+    call r14                         ; call WinExec
+    add rsp, 0x30                    ; clean up stack
+    add rsp, 0x10 
 
-        xor rdx,rdx
-        inc rdx
-        sub rsp, 0x30
-        call r14
-        add rsp, 0x30 
-        add rsp, 0x10 
-        ;add rsp, 20h
+    ; EXIT THREAD CLEANLY
+    ; next part is unecessary redundancy - finding kernel32 address, same as the first time - refer to comments above for explanation
 
-
-;  EXIT PROCESS
-
-xor rax, rax
-xor rbx, rbx
-xor rcx, rcx
-mov rax, [gs:60h + rbx] ;set rax to PEB
-mov rax, [rax+18h] ;set rbx to LDR
-mov rax, [rax+20h] ;set rcx to module list
-mov rax, [rax+rcx]
-mov rax, [rax+rcx] ;kernel32.dll
-mov rbx, [rax+20h] ;kernel32 base addr to r9
-mov r8, rbx
-mov ebx, [rbx+3ch]
-add rbx, r8 ;PE Addres
-mov rcx, 0x88FFFFFFFFFFFFFF
-shr rcx, 78h
-mov edx, [rbx + rcx]	
-add rdx, r8 ;export table addr
-mov r10d, [rdx + 14h] ;num of functions to r10
-xor r11, r11
-mov r11d, [rdx + 20h] ;function addr to r11
-add r11, r8
-xor rcx, rcx
-
-;r8 ntdll base addr
-;r10 number of functions
-;r11 addr of function names
-
-mov rcx, r10
-mov rax, 737365726464FFFFh
-shr rax, 10h
-push rax
-mov rax, 41636F7250746547h
-push rax
-mov rax, [rsp]
-findgetproc:
-    jecxz foundgetproc
-    xor ebx, ebx
-    mov ebx, [r11+4+rcx*4]
+    xor rax, rax
+    xor rbx, rbx
+    xor rcx, rcx
+    mov rax, [gs:60h + rbx]
+    mov rax, [rax+18h]
+    mov rax, [rax+20h]
+    mov rax, [rax+rcx]
+    mov rax, [rax+rcx]
+    mov rbx, [rax+20h]
+    mov r8, rbx
+    mov ebx, [rbx+3ch]
     add rbx, r8
-    dec rcx
-    cmp [rbx], rax
-    jnz findgetproc
-
-foundgetproc:
+    mov rcx, 0x88FFFFFFFFFFFFFF
+    shr rcx, 78h
+    mov edx, [rbx + rcx]	
+    add rdx, r8
+    mov r10d, [rdx + 14h]
     xor r11, r11
-    mov r11d, [rdx+0x24]
+    mov r11d, [rdx + 20h]
     add r11, r8
-    inc rcx
-    mov r13w, [r11+rcx*2]
-    xor r11, r11
-    mov r11d, [rdx+0x1c]
-    add r11, r8
-    mov eax, [r11+4+r13*4]
-    add rax, r8
-    mov r14, rax
+    xor rcx, rcx
 
-xor rcx, rcx
-xor rdx, rdx
-mov rcx, r10
-mov rax, 6461FFFFFFFFFFFFh
-shr rax, 70h
-push rax
-mov rax, 6572685474697845h
-push rax
-mov rdx, rsp
-mov rcx, r8
-sub rsp, 30h
-call r14
-add rsp, 30h
-add rsp, 10h
-mov r15, rax
-xor rcx, rcx
-xor rdx, rdx
-sub rsp, 30h
-call r15
+    mov rcx, r10                     
+    mov rax, 737365726464FFFFh       ; move "ddress"FFFF into rax
+    shr rax, 10h                     ; shorten string to remove FF, avoiding null bytes
+    push rax                         ; push onto stack
+    mov rax, 41636F7250746547h       ; move "GetProcA" into rax
+    push rax                         ; push onto stack
+    mov rax, [rsp]                   ; move "GetProcAddress" pointer from stack to rax
+    findgetproc:                     ; loop until address of GetProcAddress is found
+        jecxz foundgetproc
+        xor ebx, ebx
+        mov ebx, [r11+4+rcx*4]
+        add rbx, r8
+        dec rcx
+        cmp [rbx], rax
+        jnz findgetproc
+
+    foundgetproc:                    ; when GetProcAddress is found
+        xor r11, r11
+        mov r11d, [rdx+0x24]
+        add r11, r8
+        inc rcx
+        mov r13w, [r11+rcx*2]
+        xor r11, r11
+        mov r11d, [rdx+0x1c]
+        add r11, r8
+        mov eax, [r11+4+r13*4]
+        add rax, r8
+        mov r14, rax                 ; move GetProcAddress function to r14
+
+    xor rcx, rcx
+    xor rdx, rdx
+    mov rax, 6461FFFFFFFFFFFFh       ; move "ad"FFFFFFFFFFFF into rax
+    shr rax, 70h                     ; shorten string to remove FF, avoiding null bytes
+    push rax                         ; push onto stack
+    mov rax, 6572685474697845h       ; move "ExitThre" into rax
+    push rax                         ; push onto stack
+    mov rdx, rsp                     ; move "ExitThread" from stack to rdx for parameter
+    mov rcx, r8                      ; move kernel32.dll base address to rcx for parameter
+    sub rsp, 30h        
+    call r14                         ; call GetProcAddress(kernel32.dll, ExitThread)
+    add rsp, 30h
+    add rsp, 10h
+    mov r15, rax                     ; move ExitThread function (returned to rax by GetProcAddress) to r15
+    xor rcx, rcx                     ; clear rcx for parameter
+    sub rsp, 30h
+    call r15                         ; call ExitThread(0)
